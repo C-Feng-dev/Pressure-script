084c630587deb07a107b96fe09431486558f94eb
C-Feng-Dev <194732421@qq.com>
2024-11-24 01:35:46 +0800

    uodate

--- Pressure-loadstring.lua
+++ Pressure-loadstring.lua
@@ -1,15 +1,15 @@
 print("---------------------------加载中---------------------------")
 local function loadscript(url)
-    print("---------------------------注入中---------------------------")
+print("---------------------------注入中---------------------------")
     loadstring(game:HttpGet(url))()
 end
 if game.PlaceId == 12411473842 then
     loadscript("https://raw.githubusercontent.com/C-Feng-dev/Pressure-script/refs/heads/main/Pressure-Lobby.lua")
 elseif game.PlaceId == 17355897213 then
     loadscript("https://raw.githubusercontent.com/C-Feng-dev/Pressure-script/refs/heads/main/Pressure-The%20Raveyard.lua")
-else
+elseif game.PlaceId == 12552538292 then
     loadscript("https://raw.githubusercontent.com/C-Feng-dev/Pressure-script/refs/heads/main/Pressure.lua")
-    if game.PlaceId ~= 12552538292 then
-        warn("---------疑似未在正确游戏内注入,可能会导致报错---------")
-    end
+else
+    warn("疑似未在正确游戏内注入,可能会导致报错,已加载默认配置")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/C-Feng-dev/Pressure-script/refs/heads/main/Pressure.lua"))()
 end
